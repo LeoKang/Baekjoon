d4b58efd76264172f43e890a2abb804eb7264333
Administrator <Administrator@User-2022ILFYWR>
2022-11-10 12:45:30 +0900

    2438

---
+++ Baekjoon/src/q2438/Main.java
@@ -0,0 +1,20 @@
+package q2438;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		sc.close();
+
+//		System.out.println(N);
+
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j <= i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
